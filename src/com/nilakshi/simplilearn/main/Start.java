2682cff979bf4888d9b68b48bf33ae2ac0494570
Nilakshi-coder <55271036+Nilakshi-coder@users.noreply.github.com>
2021-07-06 14:45:16 +0530

    Initial commit

---
+++ src/com/nilakshi/simplilearn/main/Start.java
@@ -0,0 +1,13 @@
+package com.nilakshi.simplilearn.main;
+
+public class Start {
+
+	public static void main(String[] args) {
+		System.out.println("*************************************************************");
+		System.out.println("******************* Welcome to EXPLORER *********************");
+		System.out.println("**************** Devloped By NILAKSHI PATIL *****************");
+		System.out.println("*************************************************************");
+
+	}
+
+}
